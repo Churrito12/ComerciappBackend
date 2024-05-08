684aff4dba97170b73dd423828787e92248dcd7b
Ariel <ariel-dario2010@hotmail.com>
2024-05-08 16:53:12 -0300

    mercadopago agregado

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 import express from "express";
 import cors from "cors";
-//Conexion a la base de datos
 import db from "./database/db.js";
-//importe el enrutador
-
 import ProductoModel from "./models/ProductoModel.js";
 import routesUser from "./routes/routesUser.js";
 import routesProducts from "./routes/routesProducts.js";
+import { MercadoPagoConfig, Preference } from "mercadopago";
+
+const client = new MercadoPagoConfig({
+  accessToken:
+    "TEST-5097898960239229-050714-ff3d32fd6a431c07a116460257847cb5-721726567",
+});
+
 const app = express();
 
 app.use(cors());
@@ -26,6 +30,36 @@ app.get("/", (req, res) => {
 app.listen(8000, () => {
   console.log("Servidor corriendo en http://localhost:8000/");
 });
+app.post("/payment", async (req, res) => {
+  try {
+    const body = {
+      items: [
+        {
+          title: req.body.title,
+          quantity: Number(req.body.quantity),
+          unit_price: Number(req.body.price),
+          currency_id: "ARS",
+        },
+      ],
+      back_urls: {
+        success: "http://localhost:8000/",
+        failure: "",
+        pending: "",
+      },
+      auto_return: "approved",
+    };
+    const preference = new Preference(client);
+    const result = await preference.create({ body });
+    res.json({
+      id: result.id,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      error: "Error del servidor",
+    });
+  }
+});
 
 const productos = await ProductoModel.findAll({
   attributes: ["id", "stock", "stockMin", "nombre"],
@@ -42,5 +76,5 @@ productos.forEach((producto) => {
     nombre: producto.dataValues.nombre,
   };
 });
-console.log(productoMinStock);
+
 export { productosStock, productoMinStock };
